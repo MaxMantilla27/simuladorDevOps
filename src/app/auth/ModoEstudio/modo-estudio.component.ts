d6cf04eb0746f86b8e1187891e2467e9dd29eadf
Adriana Chipana <64762490+adrijonas16@users.noreply.github.com>
2023-04-11 16:26:26 -0500

    revertir

--- src/app/auth/ModoEstudio/modo-estudio.component.ts
+++ src/app/auth/ModoEstudio/modo-estudio.component.ts
@@ -57,12 +57,15 @@ export class ModoEstudioComponent implements OnInit {
   public ResultadosPorDominio:any;
   public BotonResgistrar=false;
   public Take=0;
+  public ListaLogo: any = []
+  
   ngOnInit(): void {
     this.ListaDominioCombo();
     this.ListaExamenesPorModo();
     this.ListaExamenesIncompletos();
     this.ListaExamenesConcluidos();
     this.ObtenerPromedioDominioPorModo();
+    this.ObtenerLogo();
   }
 
   RegistrarExamen(){
@@ -136,6 +139,15 @@ export class ModoEstudioComponent implements OnInit {
     this._ExamenService.ObtenerPromedioDominioPorModo(1,this.Take).subscribe({
       next:(x)=>{
         this.ResultadosPorDominio=x
+      }
+    })
+  }
+
+  ObtenerLogo(){
+    this._DominioService.ObtenerLogo().subscribe({
+      next:(x)=>{
+        this.ListaLogo = x;
+        console.log(this.ListaLogo)
       }
     })
   }
